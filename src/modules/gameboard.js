b3572c1cd37052b498e84aa883735237a8fc90ca
Yuriy Koziy <yukoziy@gmail.com>
2022-01-10 13:34:38 -0500

    Gameboard factory, with tests

---
+++ src/modules/gameboard.js
@@ -0,0 +1,96 @@
+import { SingleEntryPlugin } from 'webpack';
+import Ship from './ship';
+
+const Gameboard = () => {
+  // keep track of missed attacks
+  // report whether all of their ships have been sunk
+  const board = createBoard();
+  const missedShots = [];
+  const fleet = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
+
+  function createBoard() {
+    let boardArray = new Array(10);
+    for (let i = 0; i < 10; i++) {
+      boardArray[i] = new Array(10);
+      for (let j = 0; j < 10; j++) {
+        boardArray[i][j] = {
+          shipType: undefined,
+          index: undefined,
+        };
+      }
+    }
+    return boardArray;
+  }
+
+  function isColliding(x, y, direction, shipLength) {
+    if (direction === 'horizontal') {
+      for (let i = 0; i < shipLength; i++) {
+        if (typeof board[y][x + i] !== 'undefined') {
+          if (board[y][x + i].shipType !== undefined) {
+            return true;
+          }
+        }
+      }
+    } else if (direction === 'vertical') {
+      for (let i = 0; i < shipLength; i++) {
+        if (typeof board[y + i][x] !== 'undefined') {
+          if (board[y + i][x].shipType !== undefined) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  function isWithinBounds(x, y, direction, shipLength) {
+    if (direction === 'horizontal') {
+      return x + shipLength <= 10;
+    } else if (direction === 'vertical') {
+      return y + shipLength <= 10;
+    }
+  }
+
+  function isMoveValid(x, y, direction, shipLength) {
+    return (
+      isWithinBounds(x, y, direction, shipLength) &&
+      !isColliding(x, y, direction, shipLength)
+    );
+  }
+
+  function placeShip(x, y, direction, shipType) {
+    if (!isMoveValid(x, y, direction, fleet[shipType].length)) {
+      return;
+    }
+    if (direction === 'horizontal') {
+      for (let i = 0; i < fleet[shipType].length; i++) {
+        board[y][x + i].shipType = shipType;
+        board[y][x + i].index = i;
+      }
+    } else if (direction === 'vertical') {
+      for (let i = 0; i < fleet[shipType].length; i++) {
+        board[y + i][x].shipType = shipType;
+        board[y + i][x].index = i;
+      }
+    }
+  }
+
+  function recieveAttack(x, y) {
+    if (board[y][x].shipType === undefined) {
+      missedShots.push({ x: x, y: y });
+    } else {
+      const shipAttacked = fleet[board[y][x].shipType];
+      shipAttacked.hit(board[y][x].index);
+    }
+  }
+
+  function isFleetSunk() {
+    return fleet.every((ship, index, array) => {
+      return ship.isSunk();
+    });
+  }
+
+  return { placeShip, board, recieveAttack, missedShots, fleet, isFleetSunk };
+};
+
+export default Gameboard;
---
+++ test/gameboard.test.js
@@ -0,0 +1,147 @@
+import { createTestScheduler } from 'jest';
+import Gameboard from '../src/modules/gameboard';
+
+describe('Gameboard', () => {
+  test('Creating a gameboard, returns 2D array 10x10. 100 squares', () => {
+    const gameboard = Gameboard();
+    expect(gameboard.board.length * gameboard.board[0].length).toEqual(100);
+  });
+
+  describe('placeShip() on empty board without collisions', () => {
+    let testBoard = Gameboard();
+
+    beforeEach(() => {
+      testBoard = Gameboard();
+    });
+
+    it('should place the carrier (5 squares) horizontally at (2, 1)', () => {
+      testBoard.placeShip(2, 1, 'horizontal', 0);
+      expect(testBoard.board[1][6]).toMatchObject({
+        shipType: 0,
+        index: 4,
+      });
+    });
+
+    test('should place the carrier (5 squares) vartically at (2, 1)', () => {
+      testBoard.placeShip(2, 1, 'vertical', 0);
+      expect(testBoard.board[5][2]).toMatchObject({
+        shipType: 0,
+        index: 4,
+      });
+    });
+  });
+
+  describe('placeShip() with collisions', () => {
+    let testBoard = Gameboard();
+
+    beforeEach(() => {
+      testBoard = Gameboard();
+    });
+
+    it('should not place the carrier (5 squares) horizontally at (6, 1) because of border collision', () => {
+      testBoard.placeShip(6, 1, 'horizontal', 0);
+      expect(testBoard.board[1][6]).toMatchObject({
+        shipType: undefined,
+        index: undefined,
+      });
+    });
+
+    it('should not place the carrier (5 squares) vertically at (6, 1) because of border collision', () => {
+      testBoard.placeShip(7, 6, 'vertical', 0);
+      expect(testBoard.board[6][7]).toMatchObject({
+        shipType: undefined,
+        index: undefined,
+      });
+    });
+
+    // Horizontal ship (carrier 5 squares) at (2, 1), placing destroyer (2 squares) vertically at (2, 1), ship is not placed
+
+    it('should not place the destroyer (2 squares) vertically at (2, 1) because of collision with carrier', () => {
+      testBoard.placeShip(2, 1, 'horizontal', 0);
+      testBoard.placeShip(2, 1, 'vertical', 4);
+      expect(testBoard.board[2][2]).toMatchObject({
+        shipType: undefined,
+        index: undefined,
+      });
+    });
+
+    test('should not place the destroyer (2 squares) horizontally at (2, 1) because of collision with carrier', () => {
+      testBoard.placeShip(2, 1, 'horizontal', 0);
+      testBoard.placeShip(2, 1, 'horizontal', 4);
+      expect(testBoard.board[1][2]).toMatchObject({
+        shipType: 0,
+        index: 0,
+      });
+    });
+  });
+
+  describe('recieveAttack() test', () => {
+    let testBoard = Gameboard();
+
+    beforeEach(() => {
+      testBoard = Gameboard();
+    });
+
+    it('should record a miss when hitting (1, 1) since it misses the horizontal carrier (5 squares) at (2, 1)', () => {
+      testBoard.placeShip(2, 1, 'horizontal', 0);
+      testBoard.recieveAttack(1, 1);
+      expect(testBoard.missedShots).toEqual(
+        expect.arrayContaining([{ x: 1, y: 1 }]),
+      );
+    });
+
+    test('should register a hit when hitting (5, 1) to the carrier (5 squares) at (2, 1)', () => {
+      testBoard.placeShip(2, 1, 'horizontal', 0);
+      testBoard.recieveAttack(5, 1);
+      expect(testBoard.fleet[0].isHit()).toEqual(true);
+    });
+  });
+
+  describe('All five ships placed, testing isFleetSunk().', () => {
+    let board = Gameboard();
+
+    beforeEach(() => {
+      board = Gameboard();
+      board.placeShip(0, 0, 'vertical', 0);
+      board.placeShip(1, 0, 'vertical', 1);
+      board.placeShip(2, 0, 'vertical', 2);
+      board.placeShip(3, 0, 'vertical', 3);
+      board.placeShip(4, 0, 'vertical', 4);
+
+      //sink the carrier
+      board.recieveAttack(0, 0);
+      board.recieveAttack(0, 1);
+      board.recieveAttack(0, 2);
+      board.recieveAttack(0, 3);
+      board.recieveAttack(0, 4);
+
+      //sink the battleship
+      board.recieveAttack(1, 0);
+      board.recieveAttack(1, 1);
+      board.recieveAttack(1, 2);
+      board.recieveAttack(1, 3);
+
+      //sink the cruiser
+      board.recieveAttack(2, 0);
+      board.recieveAttack(2, 1);
+      board.recieveAttack(2, 2);
+
+      //sink the submarine
+      board.recieveAttack(3, 0);
+      board.recieveAttack(3, 1);
+      board.recieveAttack(3, 2);
+
+      //hit the destroyer
+      board.recieveAttack(4, 0);
+    });
+
+    it('should return false if there is still one ship left alive', () => {
+      expect(board.isFleetSunk()).toEqual(false);
+    });
+
+    it('should return true if all the ships are hit and sunk', () => {
+      board.recieveAttack(4, 1);
+      expect(board.isFleetSunk()).toEqual(true);
+    });
+  });
+});
