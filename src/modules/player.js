a28ead75aac9d1e7ed97b897d5a19e4130e1ca43
Yuriy Koziy <yukoziy@gmail.com>
2022-01-13 10:27:03 -0500

    Added player class and related tests.

---
+++ src/modules/player.js
@@ -0,0 +1,69 @@
+class Player {
+  constructor(playerName, playerType = '') {
+    this._playerName = playerName;
+    this._playerType = playerType;
+  }
+
+  set playerName(player_name) {
+    this._playerName = player_name;
+  }
+
+  set playerType(player_type) {
+    this._playerType = player_type;
+  }
+
+  get playerName() {
+    return this._playerName;
+  }
+
+  get playerType() {
+    return this._playerType;
+  }
+
+  isAi() {
+    return this._playerType === 'ai';
+  }
+
+  //belongs here?
+  #getRandomMove() {
+    let y = Math.floor(Math.random() * 10);
+    let x = Math.floor(Math.random() * 10);
+    return { x, y };
+  }
+
+  #isCellAlreadyHit(cell, enemyBoard) {
+    if (cell.shipType !== undefined) {
+      const hitIndex = cell.index;
+      const ship = enemyBoard.fleet[cell.shipType];
+      return ship.hitPositions[hitIndex] === true;
+    }
+    return false;
+  }
+
+  #cpuAttack(enemyBoard) {
+    const cpuMove = this.#getRandomMove();
+
+    const isMissed = enemyBoard.missedShots.some((coord) => {
+      if (coord.x === cpuMove.x && coord.y === cpuMove.y) {
+        return true;
+      }
+    });
+    const cell = enemyBoard.board[cpuMove.y][cpuMove.x];
+
+    if (isMissed || this.#isCellAlreadyHit(cell, enemyBoard)) {
+      this.#cpuAttack(enemyBoard);
+    } else {
+      enemyBoard.recieveAttack(cpuMove.x, cpuMove.y);
+    }
+  }
+
+  attack(x, y, enemyBoard) {
+    if (this._playerType === 'ai') {
+      this.#cpuAttack(enemyBoard);
+    } else {
+      enemyBoard.recieveAttack(x, y);
+    }
+  }
+}
+
+export default Player;
---
+++ test/player.test.js
@@ -0,0 +1,72 @@
+import Player from '../src/modules/player';
+import Gameboard from '../src/modules/gameboard';
+
+describe('Player class', () => {
+  describe('testing player vs. player game', () => {
+    let playerAlice, boardAlice, playerBob, boardBob;
+
+    beforeEach(() => {
+      playerAlice = new Player('Alice');
+      boardAlice = Gameboard();
+      playerBob = new Player('Bob');
+      boardBob = Gameboard();
+
+      boardAlice.placeShip(0, 0, 'vertical', 0);
+      boardBob.placeShip(0, 0, 'vertical', 0);
+    });
+
+    it('should allow player to attack opponents board, enemy ship hit should be recorded', () => {
+      playerAlice.attack(0, 1, boardBob);
+      expect(boardBob.fleet[0].hitPositions).toEqual(
+        expect.arrayContaining([false, true, false, false, false]),
+      );
+    });
+  });
+
+  describe('testing player vs. computer game', () => {
+    let playerAlice, boardAlice, playerBob, boardBob;
+
+    beforeEach(() => {
+      playerAlice = new Player('Alice');
+      boardAlice = Gameboard();
+      playerBob = new Player('Bob', 'ai');
+      boardBob = Gameboard();
+    });
+
+    it('should allow computer to make a random legal move (empty board)', () => {
+      playerBob.attack(null, null, boardAlice);
+      expect(boardAlice.missedShots.length).toEqual(1);
+    });
+
+    it('should allow computer to make a random legal move (every cells is a miss except 0,0)', () => {
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          if (i == 0 && j == 0) {
+            continue;
+          }
+          boardAlice.recieveAttack(i, j);
+        }
+      }
+      playerBob.attack(null, null, boardAlice);
+      expect(boardAlice.missedShots.pop()).toMatchObject({ x: 0, y: 0 });
+    });
+
+    it('should allow computer to make a random legal move, the only valid move should be 0,0', () => {
+      boardAlice.placeShip(0, 0, 'vertical', 0);
+      for (let i = 0; i < 10; i++) {
+        for (let j = 0; j < 10; j++) {
+          if (i == 0 && j == 1) {
+            continue;
+          }
+          boardAlice.recieveAttack(i, j);
+        }
+      }
+
+      playerBob.attack(null, null, boardAlice);
+
+      expect(
+        boardAlice.fleet[boardAlice.board[1][0].shipType].hitPositions[1],
+      ).toEqual(true);
+    });
+  });
+});
