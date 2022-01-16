3bfdf609ba7a645a700c7e7a7f74ff9f9ce3e27b
Yuriy Koziy <yukoziy@gmail.com>
2022-01-16 13:28:58 -0500

    fixed index.js, to use the new structure

--- src/index.js
+++ src/index.js
@@ -1,38 +1,6 @@
 import './style/style.scss';
-import GameboardDOM from './modules/dom/gameboardDom';
-import Gameboard from './modules/gameboard';
-import Game from './modules/game';
-import Player from './modules/player';
+import App from './modules/app';
 
-const playerBoardElement = '#player-board-container .game-board';
-const enemyBoardElement = '#enemy-board-container .game-board';
+const battleship = App();
 
-const boardPlayer = GameboardDOM(playerBoardElement);
-boardPlayer.drawEmptyBoard();
-
-const boardEnemy = GameboardDOM(enemyBoardElement);
-boardEnemy.drawEmptyBoard();
-
-const playerOne = new Player('Alice', '');
-const playerTwo = new Player('Bob', 'ai');
-
-function populateDummyBoards(boardOne, boardTwo) {
-  boardOne.placeShip(0, 0, 'vertical', 0);
-  boardOne.placeShip(3, 9, 'horizontal', 1);
-  boardOne.placeShip(3, 4, 'vertical', 2);
-  boardOne.placeShip(6, 5, 'horizontal', 3);
-  boardOne.placeShip(8, 0, 'horizontal', 4);
-  boardTwo.placeShip(0, 0, 'vertical', 0);
-  boardTwo.placeShip(3, 9, 'horizontal', 1);
-  boardTwo.placeShip(3, 4, 'vertical', 2);
-  boardTwo.placeShip(6, 5, 'horizontal', 3);
-  boardTwo.placeShip(8, 0, 'horizontal', 4);
-}
-
-const playerOneBoard = Gameboard();
-const playerTwoBoard = Gameboard();
-populateDummyBoards(playerOneBoard, playerTwoBoard);
-
-const game = Game(playerOne, playerOneBoard, playerTwo, playerTwoBoard);
-boardEnemy.addMoveHandler(game.userInput);
-game.start();
+battleship.init();
