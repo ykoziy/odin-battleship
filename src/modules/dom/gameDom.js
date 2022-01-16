dd08ae4d42cd843c961e9730060c32e0398ad319
Yuriy Koziy <yukoziy@gmail.com>
2022-01-16 13:28:20 -0500

    added gameDom

---
+++ src/modules/dom/gameDom.js
@@ -0,0 +1,15 @@
+const GameDom = (gameElementName, playerOne, playerTwo) => {
+  const gameBoard = document.querySelector(gameElementName);
+
+  function init() {
+    const nameElements = gameBoard.querySelectorAll(
+      '#player-one-name, #player-two-name',
+    );
+    nameElements[0].innerText = playerOne.playerName;
+    nameElements[1].innerText = playerTwo.playerName;
+  }
+
+  return { init };
+};
+
+export default GameDom;
