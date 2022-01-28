5b660c51b90d32ea242ab395d6abc8d647033bd3
Yuriy Koziy <yukoziy@gmail.com>
2022-01-27 19:01:31 -0500

    Added Draggable, to implement drag and drop

---
+++ src/modules/dom/draggable.js
@@ -0,0 +1,83 @@
+const Draggable = (element) => {
+  let dragged;
+  let currentDrop = null;
+
+  addEventListener();
+
+  function addEventListener() {
+    element.addEventListener('mousedown', mouseDown);
+    element.addEventListener('dragstart', (e) => e.preventDefault());
+  }
+
+  function mouseDown(e) {
+    createGhost(e.clientX, e.clientY);
+    dragged.addEventListener('mouseup', mouseUp);
+    document.addEventListener('mousemove', mouseMove);
+  }
+
+  function createGhost(x, y) {
+    dragged = element.cloneNode(true);
+    dragged.id = `dragging-${dragged.id.split('-')[1]}`;
+    dragged.style.position = 'absolute';
+    dragged.style.left = `${x}px`;
+    dragged.style.top = `${y}px`;
+    dragged.style.zIndex = '1000';
+    dragged.style.opacity = '0.5';
+    document.body.appendChild(dragged);
+  }
+
+  function moveGhost(x, y) {
+    if (x > 0 && x < window.innerWidth && y > 0 && y < window.innerHeight) {
+      dragged.style.left = `${x}px`;
+      dragged.style.top = `${y}px`;
+    } else {
+      stop();
+    }
+  }
+
+  function mouseUp(e) {
+    dragged.hidden = true;
+    let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+    dragged.hidden = false;
+    let fakeDrop = new CustomEvent('shipdropped', {
+      detail: {
+        shipType: dragged.dataset.shipType,
+      },
+    });
+    elemBelow.dispatchEvent(fakeDrop);
+    stop();
+  }
+
+  function stop() {
+    document.removeEventListener('mousemove', mouseMove);
+    document.body.removeChild(dragged);
+  }
+
+  function mouseMove(e) {
+    moveGhost(e.clientX, e.clientY);
+    dragged.hidden = true;
+    let elemBelow = document.elementFromPoint(e.clientX, e.clientY);
+    dragged.hidden = false;
+    if (elemBelow.className === 'board-cell') {
+      if (currentDrop != elemBelow) {
+        if (currentDrop) {
+          leaveTarget(currentDrop);
+        }
+        currentDrop = elemBelow;
+        if (currentDrop) {
+          enterTarget(currentDrop);
+        }
+      }
+    }
+  }
+
+  function enterTarget(el) {
+    el.style.backgroundColor = 'red';
+  }
+
+  function leaveTarget(el) {
+    el.style.backgroundColor = '';
+  }
+};
+
+export default Draggable;
