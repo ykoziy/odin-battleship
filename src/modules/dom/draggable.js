23d5b2adbab1500c800377148fed27c48d000494
Yuriy Koziy <yukoziy@gmail.com>
2022-01-28 21:28:23 -0500

    Fixed Draggable to work on mobile, rough but works.....

--- src/modules/dom/draggable.js
+++ src/modules/dom/draggable.js
@@ -1,4 +1,22 @@
 const Draggable = (element) => {
+  const scrollHeight = Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.body.clientHeight,
+    document.documentElement.clientHeight,
+  );
+
+  const scrollWidth = Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.body.clientWidth,
+    document.documentElement.clientWidth,
+  );
+
   let dragged;
   let currentDrop = null;
 
@@ -6,14 +24,19 @@ const Draggable = (element) => {
 
   function addEventListener() {
     element.addEventListener('mousedown', mouseDown);
+    element.addEventListener('touchstart', mouseDown);
+
     element.addEventListener('dragstart', preventDefault);
   }
 
   function mouseDown(e) {
     let { x, y } = getCoords(e);
     createGhost(x, y);
-    dragged.addEventListener('mouseup', mouseUp);
+    document.addEventListener('mouseup', mouseUp);
+    document.addEventListener('touchend', mouseUp);
+
     document.addEventListener('mousemove', mouseMove);
+    document.addEventListener('touchmove', mouseMove);
   }
 
   function createGhost(x, y) {
@@ -28,12 +51,21 @@ const Draggable = (element) => {
   }
 
   function moveGhost(x, y) {
-    dragged.style.left = `${x}px`;
-    dragged.style.top = `${y}px`;
+    const rect = dragged.getBoundingClientRect();
+    dragged.style.left = `${x - rect.height / 2}px`;
+    dragged.style.top = `${y - rect.height / 2}px`;
   }
 
   function mouseUp(e) {
-    let { x, y } = getCoords(e);
+    let x, y;
+    if (e.changedTouches) {
+      x = e.changedTouches[e.changedTouches.length - 1].clientX;
+      y = e.changedTouches[e.changedTouches.length - 1].clientY;
+    } else {
+      let coords = getCoords(e);
+      x = coords.x;
+      y = coords.y;
+    }
     dragged.hidden = true;
     let elemBelow = document.elementFromPoint(x, y);
     dragged.hidden = false;
@@ -42,28 +74,46 @@ const Draggable = (element) => {
         shipType: dragged.dataset.shipType,
       },
     });
-    elemBelow.dispatchEvent(fakeDrop);
+    if (elemBelow) {
+      elemBelow.dispatchEvent(fakeDrop);
+    }
     stop();
   }
 
   function stop() {
     document.removeEventListener('mousemove', mouseMove);
+    document.removeEventListener('touchmove', mouseMove);
+
+    document.removeEventListener('mouseup', mouseUp);
+    document.removeEventListener('touchend', mouseUp);
+
     document.body.removeChild(dragged);
   }
 
   function getCoords(event) {
-    return { x: event.clientX, y: event.clientY };
+    if (event.touches) {
+      if (event.touches.length != 0) {
+        return { x: event.touches[0].pageX, y: event.touches[0].pageY };
+      }
+    } else {
+      return { x: event.clientX, y: event.clientY };
+    }
   }
 
   function mouseMove(e) {
     window.getSelection().removeAllRanges();
     let { x, y } = getCoords(e);
-    if (x > 0 && x < window.innerWidth && y > 0 && y < window.innerHeight) {
+    if (x > 0 && x < scrollWidth && y > 0 && y < scrollHeight) {
       moveGhost(x, y);
+
       dragged.hidden = true;
+      if (e.touches) {
+        x = e.touches[0].clientX;
+        y = e.touches[0].clientY;
+      }
       let elemBelow = document.elementFromPoint(x, y);
       dragged.hidden = false;
-      if (elemBelow.className === 'board-cell') {
+      if (elemBelow && elemBelow.className === 'board-cell') {
         if (currentDrop != elemBelow) {
           if (currentDrop) {
             leaveTarget(currentDrop);
@@ -98,8 +148,14 @@ const Draggable = (element) => {
 
   function unset() {
     currentDrop = null;
+    document.removeEventListener('mousedown', mouseDown);
+    document.removeEventListener('touchstart', mouseDown);
+
     document.removeEventListener('mousemove', mouseMove);
-    element.removeEventListener('mousedown', mouseDown);
+    document.removeEventListener('touchmove', mouseMove);
+
+    document.removeEventListener('mouseup', mouseUp);
+    document.removeEventListener('touchend', mouseUp);
     element.removeEventListener('dragstart', preventDefault);
   }
 
